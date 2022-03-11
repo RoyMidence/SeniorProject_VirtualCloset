08b8417b545f7f52994395f1f16a8ecd5b20c841
LimitlessKieran <sylvkn@farmingdale.edu>
2022-03-10 20:44:08 -0500

    test commit

--- app/src/main/java/com/example/menu/AddClothesFragment.java
+++ app/src/main/java/com/example/menu/AddClothesFragment.java
@@ -8,7 +8,7 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-// this is the add clothes fragment
+// this is the add clothes fragment yes
 public class AddClothesFragment extends Fragment {
     @Nullable
     @Override
