74514cdfec0c0e9f4a8cf080d1136a2233d11dc3
CatheterThief <bloodslayer52798@gmail.com>
2022-03-28 14:31:10 -0400

    Updated

--- app/src/main/java/com/example/menu/TheClothesFragment.java
+++ app/src/main/java/com/example/menu/TheClothesFragment.java
@@ -1,31 +1,22 @@
 package com.example.menu;
 
-import android.content.Intent;
+
 import android.database.Cursor;
-import android.media.Image;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.SearchView;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Locale;
 
 public class TheClothesFragment extends Fragment implements ClothingAdapter.itemClickInterface {
     private DatabaseHelper mDatabaseHelper;
