2c3d9c92567506eb15b41e5594c1e883ce913ba0
CatheterThief <bloodslayer52798@gmail.com>
2022-04-10 14:57:44 -0400

    Filter system improved, now accounts for color

--- app/src/main/java/com/example/menu/FilterChosenOutfit.java
+++ app/src/main/java/com/example/menu/FilterChosenOutfit.java
@@ -13,6 +13,7 @@ import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,6 +26,7 @@ public class FilterChosenOutfit extends AppCompatActivity implements TypeAdapter
 
     ActivityResultLauncher<Intent> otherActivityLauncher;
 
+    EditText editTextFilterColor1, editTextFilterColor2;
     Button buttonAddTypes, buttonDone;
 
     @Override
@@ -33,9 +35,15 @@ public class FilterChosenOutfit extends AppCompatActivity implements TypeAdapter
         setContentView(R.layout.activity_filter_chosen_outfit);
 
         recyclerViewFilteredTypes = findViewById(R.id.recyclerViewFilteredTypes);
+        editTextFilterColor1 = findViewById(R.id.editTextFilterColor1);
+        editTextFilterColor2 = findViewById(R.id.editTextFilterColor2);
         buttonAddTypes = findViewById(R.id.buttonAddTypes);
         buttonDone = findViewById(R.id.buttonDone);
+
+        //Pass in old filters
         setUpRecycler();
+        editTextFilterColor1.setText(getIntent().getExtras().getString("c1"));
+        editTextFilterColor2.setText(getIntent().getExtras().getString("c2"));
 
 
 
@@ -72,12 +80,28 @@ public class FilterChosenOutfit extends AppCompatActivity implements TypeAdapter
             public void onClick(View view) {
                 Intent resultIntent = new Intent();
 
+                // Values for things getting filtered
                 ArrayList<String> newTypes = new ArrayList<>();
+                String c1 = "Any";
+                String c2 = "Any";
+
+                // Get list of Types
+
                 for (int i = 0; i < listTypes.size(); i++) {
                     newTypes.add(listTypes.get(i));
                 }
 
+                // Get Colors
+                if (!editTextFilterColor1.equals(null))
+                    c1 = editTextFilterColor1.getText().toString();
+
+                if (!editTextFilterColor2.equals(null))
+                    c2 = editTextFilterColor2.getText().toString();
+
+                // pack them up
                 resultIntent.putStringArrayListExtra("types",newTypes);
+                resultIntent.putExtra("c1",c1);
+                resultIntent.putExtra("c2",c2);
                 setResult(0,resultIntent);
                 finish(); // Come back later, when other screen done
             }
--- app/src/main/java/com/example/menu/RandomizeOutfit.java
+++ app/src/main/java/com/example/menu/RandomizeOutfit.java
@@ -20,6 +20,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
@@ -33,11 +34,15 @@ public class RandomizeOutfit extends Fragment  implements ClothingAdapter.itemCl
  private View v;
  private RecyclerView recyclerViewRandomOutfit;
     private List<ClothingItem> clothingItems = new ArrayList<>();
-    private ArrayList<String> types = new ArrayList<>();
+
     private DatabaseHelper mDatabaseHelper;
     private ClothingAdapter list;
     EditText outfitNameRandom;
     ActivityResultLauncher<Intent> otherActivityLauncher;
+
+    // Globals for filter parameters
+    private ArrayList<String> types = new ArrayList<>();
+    private String c1, c2;
 
     public RandomizeOutfit() {
         // Required empty public constructor
@@ -56,11 +61,15 @@ public class RandomizeOutfit extends Fragment  implements ClothingAdapter.itemCl
                              Bundle savedInstanceState) {
         v = inflater.inflate(R.layout.fragment_randomize_outfit, container, false);
 
-
-        outfitNameRandom = (EditText) v.findViewById(R.id.randomOutfit);
+        // Set Basic Default Filter Parameters
         types.add("Shirt");
         types.add("Pants");
         types.add("Shoes");
+        c1 = "Any";
+        c2 = "Any";
+
+
+        outfitNameRandom = (EditText) v.findViewById(R.id.randomOutfit);
         mDatabaseHelper = new DatabaseHelper(getContext());
         recyclerViewRandomOutfit = v.findViewById(R.id.recyclerViewRandom);
         configureButtons();
@@ -75,6 +84,9 @@ public class RandomizeOutfit extends Fragment  implements ClothingAdapter.itemCl
                             if (resultIntent != null) {
                                 types. clear();
                                 types = resultIntent.getStringArrayListExtra("types");
+                                c1 = resultIntent.getExtras().getString("c1");
+                                c2 = resultIntent.getExtras().getString("c2");
+
                             }
                         }
 
@@ -91,7 +103,7 @@ public class RandomizeOutfit extends Fragment  implements ClothingAdapter.itemCl
         recyclerViewRandomOutfit.setAdapter(list);
     }
 
-    private void chooseClothingItem(String type) {
+    private boolean chooseClothingItem(String type) {
         ClothingItem CI;
         List<ClothingItem> temp = new ArrayList<>(); // will hold all of a specific type
 
@@ -106,10 +118,39 @@ public class RandomizeOutfit extends Fragment  implements ClothingAdapter.itemCl
                         mDatabaseHelper.getOccasion(id), mDatabaseHelper.checkSpring(id), mDatabaseHelper.checkSummer(id), mDatabaseHelper.checkFall(id), mDatabaseHelper.checkWinter(id), mDatabaseHelper.checkAll(id));
                 temp.add(CI);
             }
+        } // Now have a list with the specified types
+
+        // Shorten list down to colors
+        // Only doing for shirts for now
+        if (type.equals("Shirt")) {
+            for (int i = 0; i < temp.size(); i++) {
+                if (!c1.equals("Any") && !c2.equals("Any")) { // Both colors, looking for specified combo
+                    if (!temp.get(i).getColor1().equals(c1) || !temp.get(i).getColor2().equals(c2)) {
+                        temp.remove(i);
+                        i--;
+                    }
+                } else if (!c1.equals("Any")) { // Looking for primary color
+                    if (!temp.get(i).getColor1().equals(c1)) {
+                        temp.remove(i);
+                        i--;
+                    }
+                } else  if (!c2.equals("Any")){ // Looking for secondary color
+                    if (!temp.get(i).getColor2().equals(c2)) {
+                        temp.remove(i);
+                        i--;
+                    }
+                } // No default. if there is no color filter then no need for work
+            }
+        } // Broke down list of shirts, should only be left with shirts of the correct color
+        // But also might not have shirts at all, need to account for this
+
+        if (temp.size() == 0) { // If nothing with that color exists
+            return false;
         }
 
         Random random = new Random(); // use this to generate a random number
         clothingItems.add(temp.get(random.nextInt(temp.size()))); // adding a random clothing Item here
+        return true;
     }
 
     private void configureButtons() {
@@ -121,16 +162,22 @@ public class RandomizeOutfit extends Fragment  implements ClothingAdapter.itemCl
         randomButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                boolean worked = true;
                 clothingItems.clear(); // new outfit everytime
 
                 for (int i = 0; i < types.size();i++) { // Will cycle through the types of clothing
-                    chooseClothingItem(types.get(i));
+                    if (!chooseClothingItem(types.get(i))) {
+                        Toast.makeText(getContext(), "No shirt with that color", Toast.LENGTH_SHORT).show();
+                        worked = false;
+                        break;
+                    }
                 }
 
-                list.notifyDataSetChanged();
-
-                cancel.setVisibility(View.VISIBLE);
-                save.setVisibility(View.VISIBLE);
+                if (worked) {
+                    list.notifyDataSetChanged();
+                    cancel.setVisibility(View.VISIBLE);
+                    save.setVisibility(View.VISIBLE);
+                }
 
 
             }
@@ -141,6 +188,8 @@ public class RandomizeOutfit extends Fragment  implements ClothingAdapter.itemCl
             public void onClick(View view) {
                 Intent intent = new Intent(getContext(), FilterChosenOutfit.class);
                 intent.putStringArrayListExtra("types",types);
+                intent.putExtra("c1",c1);
+                intent.putExtra("c2",c2);
                 otherActivityLauncher.launch(intent);
             }
         });
